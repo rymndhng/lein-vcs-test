83ab06655df456fec8b52a5d1cd3ece2194e7113
Raymond Huang <raymond.huang@unbounce.com>
2016-10-19 17:58:54 -0700

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-vcs-test "0.1.0"
+(defproject lein-vcs-test "0.1.1-SNAPSHOT"
   :description "Leiningen optimizations for testing with Version Controlled Systems"
   :url "https://github.com/rymndhng/lein-vcs-test"
   :license {:name "Eclipse Public License"
